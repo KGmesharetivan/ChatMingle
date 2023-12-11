4f5f9f4fc264f494c6ce7837c643640cf6a689a6
mesharetivan <146832484+KGmesharetivan@users.noreply.github.com>
2023-12-12 00:57:46 +0800

    Update functionality

---
+++ backend/server.js
@@ -0,0 +1,178 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+// Middleware for serving your React application
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.use("/uploads", express.static("uploads"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+});
+
+let connectedPeers = [];
+let connectedPeersStrangers = [];
+
+io.on("connection", (socket) => {
+  connectedPeers.push(socket.id);
+
+  socket.on("pre-offer", (data) => {
+    console.log("pre-offer-came");
+    const { calleePersonalCode, callType } = data;
+    console.log(calleePersonalCode);
+    console.log(connectedPeers);
+    const connectedPeer = connectedPeers.find(
+      (peerSocketId) => peerSocketId === calleePersonalCode
+    );
+
+    console.log(connectedPeer);
+
+    if (connectedPeer) {
+      const data = {
+        callerSocketId: socket.id,
+        callType,
+      };
+      io.to(calleePersonalCode).emit("pre-offer", data);
+    } else {
+      const data = {
+        preOfferAnswer: "CALLEE_NOT_FOUND",
+      };
+      io.to(socket.id).emit("pre-offer-answer", data);
+    }
+  });
+
+  socket.on("pre-offer-answer", (data) => {
+    const { callerSocketId } = data;
+
+    const connectedPeer = connectedPeers.find(
+      (peerSocketId) => peerSocketId === callerSocketId
+    );
+
+    if (connectedPeer) {
+      io.to(data.callerSocketId).emit("pre-offer-answer", data);
+    }
+  });
+
+  socket.on("webRTC-signaling", (data) => {
+    const { connectedUserSocketId } = data;
+
+    const connectedPeer = connectedPeers.find(
+      (peerSocketId) => peerSocketId === connectedUserSocketId
+    );
+
+    if (connectedPeer) {
+      io.to(connectedUserSocketId).emit("webRTC-signaling", data);
+    }
+  });
+
+  socket.on("user-hanged-up", (data) => {
+    const { connectedUserSocketId } = data;
+
+    const connectedPeer = connectedPeers.find(
+      (peerSocketId) => peerSocketId === connectedUserSocketId
+    );
+
+    if (connectedPeer) {
+      io.to(connectedUserSocketId).emit("user-hanged-up");
+    }
+  });
+
+  socket.on("stranger-connection-status", (data) => {
+    const { status } = data;
+    if (status) {
+      connectedPeersStrangers.push(socket.id);
+    } else {
+      const newConnectedPeersStrangers = connectedPeersStrangers.filter(
+        (peerSocketId) => peerSocketId !== socket.id
+      );
+      connectedPeersStrangers = newConnectedPeersStrangers;
+    }
+
+    console.log(connectedPeersStrangers);
+  });
+
+  socket.on("get-stranger-socket-id", () => {
+    let randomStrangerSocketId;
+    const filterConnectedPeersStrangers = connectedPeersStrangers.filter(
+      (peerSocketId) => peerSocketId !== socket.id
+    );
+
+    if (filterConnectedPeersStrangers.length > 0) {
+      randomStrangerSocketId =
+        filterConnectedPeersStrangers[
+          Math.floor(Math.random() * filterConnectedPeersStrangers.length)
+        ];
+    } else {
+      randomStrangerSocketId = null;
+    }
+
+    const data = {
+      randomStrangerSocketId,
+    };
+
+    io.to(socket.id).emit("stranger-socket-id", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+
+    const newConnectedPeers = connectedPeers.filter(
+      (peerSocketId) => peerSocketId !== socket.id
+    );
+
+    connectedPeers = newConnectedPeers;
+    //console.log(connectedPeers);
+
+    const newConnectedPeersStrangers = connectedPeersStrangers.filter(
+      (peerSocketId) => peerSocketId !== socket.id
+    );
+    connectedPeersStrangers = newConnectedPeersStrangers;
+  });
+});
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+server.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
+
+server.on("error", (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+});
