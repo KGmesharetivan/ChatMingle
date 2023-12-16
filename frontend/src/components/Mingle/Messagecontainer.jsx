330988f676b1be70d2a13449db696436ef1b482b
mesharetivan <146832484+KGmesharetivan@users.noreply.github.com>
2023-12-16 23:25:48 +0800

    update styling

--- frontend/src/components/Mingle/Messagecontainer.jsx
+++ frontend/src/components/Mingle/Messagecontainer.jsx
@@ -3,9 +3,28 @@ import React, { useState, useEffect } from "react";
 import sendMessageButtonImage from "../../assets/images/sendMessageButton.png";
 import * as store from "../../assets/js/store";
 import * as constants from "../../assets/js/constants";
+import * as webRTCHandler from "../../assets/js/webRTCHandler";
+import * as ui from "../../assets/js/ui";
 
 const MessageContainer = () => {
   const callType = store.getState().callType;
+
+  const handleKeyDown = (event) => {
+    const key = event.key;
+
+    if (key === "Enter") {
+      webRTCHandler.sendMessageUsingDataChannel(event.target.value);
+      ui.appendMessage(event.target.value, true);
+      event.target.value = "";
+    }
+  };
+
+  const handleClick = () => {
+    const message = document.getElementById("new_message_input").value;
+    webRTCHandler.sendMessageUsingDataChannel(message);
+    ui.appendMessage(message, true);
+    document.getElementById("new_message_input").value = "";
+  };
 
   return (
     <div className="messenger_container">
@@ -18,8 +37,13 @@ const MessageContainer = () => {
               id="new_message_input"
               type="text"
               placeholder="Type your message..."
+              onKeyDown={handleKeyDown}
             />
-            <button className="send_message_button" id="send_message_button">
+            <button
+              className="send_message_button"
+              id="send_message_button"
+              onClick={handleClick}
+            >
               <img
                 className="send_message_button_image"
                 src={sendMessageButtonImage}
