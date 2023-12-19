cb815737ca34875869f9df3cf678c96c338dbe3d
khinsanity <holycrapzz@yahoo.com>
2023-12-20 03:57:46 +0800

    Update profile
    Hindi ko sure kung ganitong yung idea niya pero let me know.

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,8 +1,26 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React from 'react'
 
 const Profile = () => {
-  return <div>Profile</div>;
-};
+  return (
+    <div className='bg-gray-100 min-h-min'>
+      {/* Cover Photo */}
+      <div
+        className='h-80 bg-cover bg-center relative'
+        style={{ backgroundImage: 'url(https://lh3.googleusercontent.com/ledvgumQmauwI-C2mKr31nAJ4rVLkNz3GAz8fpufS2HdNXVOKVQ1bLOuqYg_kJ8XWiYW9fovz298cBjknYB0sMwpiu6uG8tlpw=w1200-h600-pp-rj-s365)'}}
+      >
+        {/* Profile Picture */}
+        <div className='h-44 w-44 bg-white rounded-full border-4 border-white absolute -bottom-10 left-36 transform translate-x-1/2 -trnaslate-y-1/2'>
+          {/* You can add an antual profile Picture here */}
+        </div>
+      </div>
+
+      {/* User Information */}
+      <div className='max-w-2xl mx-auto px-5 mt-4'>
+        <h1 className='text-3xl font-bold text-gray-800'>Jorz Tandog</h1>
+      </div>
+    </div>
+  )
+}
 
 export default Profile;
