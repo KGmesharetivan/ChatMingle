389dc3a17f1ea5f5aef01310a6c0a0dd44ddd7c0
mesharetivan <146832484+KGmesharetivan@users.noreply.github.com>
2024-01-09 04:35:21 +0800

    For deployment

--- frontend/socket.js
+++ frontend/socket.js
@@ -2,7 +2,7 @@
 
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001/");
+const socket = io("https://wihwxepmb2.ap-southeast-1.awsapprunner.com/");
 
 // Export the socket instance for use in your components.
 export default socket;
