fe48063267883ab177c0fee6fd9665e8df07c0ef
mesharetivan <146832484+KGmesharetivan@users.noreply.github.com>
2024-01-08 22:50:56 +0800

    Update

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 // Socket.IO configuration
 const io = require("socket.io")(server, {
   cors: {
-    origin: "*",
+    origin: "https://chatmingle--bright-cascaron-41cee7.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   },
 });
@@ -30,7 +30,7 @@ const io = require("socket.io")(server, {
 // Use cors middleware
 app.use(
   cors({
-    origin: "*",
+    origin: "https://chatmingle--bright-cascaron-41cee7.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
