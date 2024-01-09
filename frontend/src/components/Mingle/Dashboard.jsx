b073429c1e5c9a9ba166936178715ab5742cbe24
mesharetivan <146832484+KGmesharetivan@users.noreply.github.com>
2024-01-09 18:10:42 +0800

    For Deployment

--- frontend/src/components/Mingle/Dashboard.jsx
+++ frontend/src/components/Mingle/Dashboard.jsx
@@ -26,7 +26,7 @@ function Dashboard({ toast }) {
 
   useEffect(() => {
     // Initialize Socket.IO client
-    const newSocket = io("http://localhost:3001"); // Replace with your server URL
+    const newSocket = io("https://wihwxepmb2.ap-southeast-1.awsapprunner.com"); // Replace with your server URL
     setSocket(newSocket);
 
     // Register socket events
